e891597d32384ebb0de17a9d9c8c94a1cbf54981
Tanapat Fasaisirinun <tanapat23fsn@gmail.com>
2018-08-30 17:01:38 +0700

    START HERE

---
+++ test1.py
@@ -0,0 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+url_to_scrape = 'https://th.wikipedia.org/wiki/%E0%B8%AB%E0%B8%99%E0%B9%89%E0%B8%B2%E0%B8%AB%E0%B8%A5%E0%B8%B1%E0%B8%81'
+r = requests.get(url_to_scrape)
+soup = BeautifulSoup(r.text, "lxml")
+data=soup.select("li[id='n-mainpage'] a")
+for i in data:
+            print(i.text.strip())
+
+
