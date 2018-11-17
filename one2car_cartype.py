929a3f03707c24f0db28d3bf6bbc17764145a13e
Tanapat Fasaisirinun <tanapat23fsn@gmail.com>
2018-11-17 17:55:56 +0700

    แก้ไขชื่อตัวแปรเพื่อความเข้าใจ

--- one2car_cartype.py
+++ one2car_cartype.py
@@ -1,13 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
-import one2car_datacar
-data_keeplink=[]
+import one2car_cardata
+keep_sendlink=[]
 url_to_scrape = 'https://www.one2car.com/%E0%B8%A3%E0%B8%96-%E0%B8%AA%E0%B8%B3%E0%B8%AB%E0%B8%A3%E0%B8%B1%E0%B8%9A-%E0%B8%82%E0%B8%B2%E0%B8%A2?type=used&page_number=1&page_size=25'
 r = requests.get(url_to_scrape)
 soup = BeautifulSoup(r.text, "lxml")
-data_linkcar = soup.select("a.ellipsize")
-data_numallcar = soup.select("h1.headline.delta.flush")
-for i in data_numallcar:
+url_linkcar = soup.select("a.ellipsize")
+num_allcar = soup.select("h1.headline.delta.flush")
+for i in num_allcar:
     k=i.text.strip().split(" ")
     k=k[2]
     k=k.strip().split(",")
@@ -19,12 +19,10 @@ while(num != maxpage):
     url_to_scrape = 'https://www.one2car.com/%E0%B8%A3%E0%B8%96-%E0%B8%AA%E0%B8%B3%E0%B8%AB%E0%B8%A3%E0%B8%B1%E0%B8%9A-%E0%B8%82%E0%B8%B2%E0%B8%A2?type=used&page_number='+str(num)+'&page_size=25'
     r = requests.get(url_to_scrape)
     soup = BeautifulSoup(r.text, "lxml")
-    data_linkcar = soup.select("a.ellipsize")
-    data_numallcar = soup.select("h1.headline.delta.flush")
-    for i in data_linkcar:
-        data_keeplink.append(i['href'])
+    url_linkcar = soup.select("a.ellipsize")
+    num_allcar = soup.select("h1.headline.delta.flush")
+    for i in url_linkcar:
+        keep_sendlink.append(i['href'])
 
-for i in data_keeplink:
-    one2car_datacar.sendlinks(i)
-
-
+for i in keep_sendlink:
+    one2car_cardata.Main(i)
