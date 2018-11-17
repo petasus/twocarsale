34a78bb4e679d19604c1a8e0f086354cfb3f7728
Tanapat Fasaisirinun <tanapat23fsn@gmail.com>
2018-11-17 17:58:21 +0700

    ลบไฟล์

--- img_car.py
+++
@@ -1,6 +0,0 @@
-import webbrowser
-from urllib.request import urlopen
-def read_image_url(url,num):
-    with urlopen(url) as response, open("img"+str(num)+".jpg", "wb") as out_file:
-        data = response.read()
-        out_file.write(data)
