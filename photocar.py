47c19b8d079a7c11ed5aa8f0de9fc93da77d01e5
Godsxy <kawkarm@gmail.com>
2019-02-14 13:59:54 +0700

    update photocar now when get photo it can send to up.php for upload to web server.

--- photocar.py
+++ photocar.py
@@ -1,21 +1,22 @@
-import webbrowser
+import requests
+import os
 from urllib.request import urlopen
 
-url = ('https://img5.icarcdn.com/6245184/gallery_used-car-one2car-ford-ranger-hi-rider-xlt-pickup-thailand_6245184_hcHqikZVX81h4Wgd90Bvgh.jpg?smia=xTM')
-
-def read_image_url(url,num):
-    with urlopen(url) as response, open("img"+str(num)+".jpg", "wb") as out_file:
+def read_image_url(url,id,order):
+    name = "imgID_"+str(id)+"_Order_"+order+".jpg"
+    with urlopen(url) as response, open(name, "wb") as out_file:
         data = response.read()
         out_file.write(data)
+        out_file.close()
+    link='http://127.0.0.1/test/up.php'
+    file_up=open(name, 'rb')
+    files = {'file':file_up}
+    requests.post(link, files=files)
+    file_up.close()
+    os.remove(name)
 
+url = ('https://img5.icarcdn.com/6245184/gallery_used-car-one2car-ford-ranger-hi-rider-xlt-pickup-thailand_6245184_hcHqikZVX81h4Wgd90Bvgh.jpg?smia=xTM')
+num = 1
+name = "img"+str(num)+".jpg"
 
-
-#r = requests.get(url_to_scrape).content
-#soup = BeautifulSoup(r, "lxml")
-#    image_tags = soup.select("div img.gallery__item")
-#    number_name=1
-#    for i in image_tags:
-#        img_car.read_image_url(i.get('data-src'), number_name)
-#        number_name=number_name+1
-
-read_image_url(url,1)
+read_image_url(url,num)
---
+++ up.php
@@ -0,0 +1,6 @@
+555
+
+<?php
+print_r($_FILES);
+move_uploaded_file($_FILES["file"]["tmp_name"],$_FILES["file"]["name"]);
+?>
