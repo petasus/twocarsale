a41b918a6ad0a0116ca472e6c931729d51fd7be0
Tanapat Fasaisirinun <tanapat23fsn@gmail.com>
2019-05-07 18:24:24 +0700

    ปล่อยไว้ เผื่อมีประโยชน์

--- usedcars_all_photo.py
+++ usedcars_all_photo.py
@@ -16,7 +16,8 @@ def read_image_url(url,id,order):
     os.remove(name)
 
 url = ('https://img5.icarcdn.com/6245184/gallery_used-car-one2car-ford-ranger-hi-rider-xlt-pickup-thailand_6245184_hcHqikZVX81h4Wgd90Bvgh.jpg?smia=xTM')
+
 num = 1
-name = "img"+str(num)+".jpg"
+name = "img"+str(id)+"_"+str(num)+".jpg"
 
 read_image_url(url,num)
