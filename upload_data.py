8c30963b61b3650dbbb1d1f5bff8acf0fbb6172c
Tanapat Fasaisirinun <tanapat23fsn@gmail.com>
2019-06-12 15:23:27 +0700

    no message

--- upload_data.py
+++ upload_data.py
@@ -14,7 +14,7 @@ def uploadToSql(CarDetail):
         print(i)
         try:
             c.execute("SET NAMES utf8mb4;")
-            sql = """INSERT INTO main_data (`mod_id`, `color`, `price`, `years`, `mile`, `seller`, `tel`, `loc`, `date`) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
+            sql = """INSERT INTO main_data (`mod_id`, `color`, `price`, `years`, `mile`, `seller`, `tel`, `loc`, `date`, `img_name`) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
             #เช็คข้อมูลซ้ำ
             CKsql = """SELECT * FROM main_data WHERE `mod_id`=%s AND `color`=%s AND `price`=%s AND `years`=%s AND `mile`=%s AND `seller`=%s"""
             CKExis = c.execute(CKsql,(i["""mod"""],i["""col"""],i["""pri"""],i["""yea"""],i["""mil"""],i["""nam"""],))
@@ -23,7 +23,7 @@ def uploadToSql(CarDetail):
                 print("Duplicate Data in List..")
             else:
                 print(27)
-                c.execute(sql, (i["""mod"""],i["""col"""],i["""pri"""],i["""yea"""],i["""mil"""],i["""nam"""],i["""tel"""],i["""loc"""],i["""dat"""]))
+                c.execute(sql, (i["""mod"""],i["""col"""],i["""pri"""],i["""yea"""],i["""mil"""],i["""nam"""],i["""tel"""],i["""loc"""],i["""dat"""],i["""img"""]))
                 db.commit()
 
         except Exception as e:
